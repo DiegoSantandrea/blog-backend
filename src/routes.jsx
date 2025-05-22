706db4334352ad195680830d06a5e399c22372fc
santandrea <diegotata2006@gmail.com>
2025-05-21 19:21:48 -0600

    se eliminaron algunos archivos que eran del fronten y se subieron accidentalmente al backen

--- src/routes.jsx
+++
@@ -1,8 +0,0 @@
-import { PublicationView } from './componentes/publication/PublicationView';
-import { DashboardPage } from "./pages/dashboard"
-import { obtenerPublicaciones } from './servicios';
-
-export const routes = [
-    { path: '/', element: <DashboardPage /> },
-    { path: '/publication/:id', element: <PublicationView obtenerPublicaciones={obtenerPublicaciones} /> },
-]
